4b827962e5811c898942db933630948a3a6cc759
Claudio Jin <natsu165@hotmail.com>
2023-01-12 16:49:10 -0300

    getter e setter de referencia

--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -6,5 +6,21 @@ public class TestaGetESet {
 		
 		conta.setNumero(1377);
 		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		paulo.setNome("Paulo Silveira");
+		conta.setTitular(paulo);
+	
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		
+		//agora com duas linhas
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programador java");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
 	}
 }
