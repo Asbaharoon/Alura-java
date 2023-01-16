58d8a98f3e516dbf435e45fb39ad55d143810a4d
Claudio Jin <natsu165@hotmail.com>
2023-01-16 14:21:42 -0300

    bytebank herança

---
+++ bytebank-herdado-conta/src/TestaContas.java
@@ -0,0 +1,33 @@
+
+public class TestaContas {
+
+	public static void main(String[] args) {
+
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100.00);
+		
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
+		cp.deposita(200.00);
+		
+		
+		cc.transfere(10.0, cp);
+		
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
+		
+		
+//		Conta conta = new Conta(1337, 23334);
+//
+//		conta.deposita(200.0);
+//
+//		System.out.println(conta.getSaldo());
+//
+//		conta.setAgencia(570);
+//
+//		System.out.println(conta.getAgencia());
+//
+//		System.out.println("o total de contas é : " + Conta.getTotal());
+
+	}
+
+}
