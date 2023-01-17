3136b7bfcdc47d0d9df2bfd6acc000d75699c79a
Claudio Jin <natsu165@hotmail.com>
2023-01-17 12:58:58 -0300

    implementacao de interfaces

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,10 +1,26 @@
 
-public class Gerente extends FuncionarioAutenticavel{
+public class Gerente extends Funcionario implements Autenticavel{
 
+	private int senha;
+	
 	//reutilizando métodos da classe pai
 	//referenciamos com a palavra super
 	public double getBonificacao() {
 		System.out.println("Chamando o metodo do gerente");
 		return super.getSalario();
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;;
 	}
 }
