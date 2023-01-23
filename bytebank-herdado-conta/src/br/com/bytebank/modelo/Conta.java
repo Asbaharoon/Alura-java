b59cbd6045d5858b36de46640769ee0f5533c6ad
Claudio Jin <natsu165@hotmail.com>
2023-01-23 18:45:37 -0300

    comparable/comparator

--- bytebank-herdado-conta/src/br/com/bytebank/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.modelo;
  * @author claudio jin
  *
  */
-public abstract class Conta {
+public abstract class Conta extends Object implements Comparable<Conta>{
 
 	protected double saldo;
 	private int agencia;
@@ -111,8 +111,14 @@ public abstract class Conta {
 	}
 	
 	@Override
+	public int compareTo(Conta outra) {
+		
+		return Double.compare(this.saldo, outra.saldo);
+	}
+	
+	@Override
 	public String toString() {
-		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
+		return "Numero: " + this.numero + ", Agencia: " + this.agencia + ", Saldo: " + this.saldo;
 	}
 
 }
