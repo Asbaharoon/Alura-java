c47ab9a28457101d30dda77b93ad3fb49145bfb0
Claudio Jin <natsu165@hotmail.com>
2023-01-31 18:03:29 -0300

    comentarios

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -29,6 +29,12 @@ public class Funcionario {
 	}
 
 	public void reajustarSalario(BigDecimal reajuste) {
-        this.salario = this.salario.add(reajuste).setScale(2, RoundingMode.HALF_UP);
+        this.salario = this.salario.add(reajuste);
+        arredondarSalario();
     }
+
+	//nao testar metodos privados
+	private void arredondarSalario() {
+		this.salario = this.salario.setScale(2, RoundingMode.HALF_UP);
+	}
 }
--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -4,7 +4,10 @@ import java.math.BigDecimal;
 
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
-
+/*
+ * as classes que possuem regras de negocios
+ * e muitas mudancas, normalmente sao as que precisam de mais testes
+ */
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
