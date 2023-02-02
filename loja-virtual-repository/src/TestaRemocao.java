ad57a7eb40df399542ed6dc00633207f125a5fa4
Claudio Jin <natsu165@hotmail.com>
2023-02-02 17:59:04 -0300

    removendo dados do banco

---
+++ loja-virtual-repository/src/TestaRemocao.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection con = factory.recuperarConexao();
+		
+		Statement stm = con.createStatement();
+		stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+		
+		/*
+		 * retorna quantas linhas modificadas
+		 * 
+		 */
+		Integer linhasModificadas = stm.getUpdateCount();
+	
+		System.out.println("Quantidade de linhas que foram modificadas: " + linhasModificadas);
+	}
+}
