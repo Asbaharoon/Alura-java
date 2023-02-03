80419978589c1e0e8799107f44c0b2da73aa6573
Claudio Jin <natsu165@hotmail.com>
2023-02-03 15:47:05 -0300

    dao de categorias

--- loja-virtual-repository/src/br/com/alura/jdbc/TestaEListagemInsercaoComProduto.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaEListagemInsercaoComProduto.java
@@ -2,11 +2,12 @@ package br.com.alura.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaEListagemInsercaoComProduto {
 
 	public static void main(String[] args) throws SQLException {
 		Produto comoda = new Produto("Comoda", "comoda vertical");
@@ -16,7 +17,9 @@ public class TestaInsercaoComProduto {
 			
 			dao.salvar(comoda);
 			
-			dao.listar();
+			List<Produto> listaDeProdutos = dao.listar();
+		
+			listaDeProdutos.forEach(p -> System.out.println(p));
 		}
 	}
 }
